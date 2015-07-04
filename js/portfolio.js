6af477ef40b1bd8fc4b5c09b2b7972ac86fe5cf3
Noel Negusse <noel.blob@gmail.com>
2015-07-04 00:14:48 -0500

    added hover image via js

---
+++ js/portfolio.js
@@ -0,0 +1,26 @@
+
+function g_hover(element) {
+    element.setAttribute('src', 'img/github-logo2.png');
+}
+
+function g_unhover(element) {
+    element.setAttribute('src', 'img/github-logo.png');
+}
+
+
+function t_hover(element) {
+    element.setAttribute('src', 'img/twitter-logo2.png');
+}
+
+function t_unhover(element) {
+    element.setAttribute('src', 'img/twitter-logo.png');
+}
+
+
+function l_hover(element) {
+    element.setAttribute('src', 'img/linkedin-logo2.png');
+}
+
+function l_unhover(element) {
+    element.setAttribute('src', 'img/linkedin-logo.png');
+}
