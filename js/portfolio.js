6c3b93c51c0a32a326fc881d0c838c3c89fbaff6
Noel Negusse <noel.blob@gmail.com>
2015-07-07 14:24:21 -0500

    further refined project icons and images

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,26 +1,75 @@
+/* ##### SOCIAL MEDIA LOGOS ##### */
 
-function g_hover(element) {
-    element.setAttribute('src', 'img/github-logo2.png');
+// Github
+function g_hover(element) 
+{
+	element.setAttribute('src', 'img/github-logo2.png');
 }
 
-function g_unhover(element) {
-    element.setAttribute('src', 'img/github-logo.png');
+function g_unhover(element) 
+{
+	element.setAttribute('src', 'img/github-logo.png');
+}
+//Twitter
+function t_hover(element) 
+{
+	element.setAttribute('src', 'img/twitter-logo2.png');
 }
 
-
-function t_hover(element) {
-    element.setAttribute('src', 'img/twitter-logo2.png');
+function t_unhover(element) 
+{
+	element.setAttribute('src', 'img/twitter-logo.png');
+}
+//Linkedin
+function l_hover(element) 
+{
+	element.setAttribute('src', 'img/linkedin-logo2.png');
 }
 
-function t_unhover(element) {
-    element.setAttribute('src', 'img/twitter-logo.png');
+function l_unhover(element) 
+{
+	element.setAttribute('src', 'img/linkedin-logo.png');
 }
 
+/* ##### PROJECT ICONS ##### */
 
-function l_hover(element) {
-    element.setAttribute('src', 'img/linkedin-logo2.png');
+//Space Invaders icon
+function si_hover(element) 
+{
+	element.setAttribute('src', 'img/projects/space_invader2.jpg');
 }
 
-function l_unhover(element) {
-    element.setAttribute('src', 'img/linkedin-logo.png');
+function si_unhover(element) 
+{
+	element.setAttribute('src', 'img/projects/space_invader.jpg');
+}
+//gear
+function gear_hover(element) 
+{
+	element.setAttribute('src', 'img/projects/gear2.jpg');
+}
+
+function gear_unhover(element) 
+{
+	element.setAttribute('src', 'img/projects/gear.jpg');
+}
+//finger poke
+function poke_hover(element) 
+{
+	element.setAttribute('src', 'img/projects/poke2.png');
+}
+
+function poke_unhover(element) 
+{
+	element.setAttribute('src', 'img/projects/poke.png');
+}
+//microchip
+function microchip_hover(element) 
+{
+	element.setAttribute('src', 'img/projects/microchip2.png');
+}
+
+function microchip_unhover(element) 
+{
+	element.setAttribute('src', 'img/projects/microchip.png');
 }
