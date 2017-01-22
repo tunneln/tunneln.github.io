c406e30a6bfc42192128ed2c7eebc94b49322205
Noel Negusse <noel.blob@gmail.com>
2017-01-22 06:31:54 -0600

    Added projects section and cleaned up a lot of unecessary junk

--- js/portfolio.js
+++ js/portfolio.js
@@ -15,7 +15,8 @@ function g_unhover(element, github)
 	element.setAttribute('src', 'img/logos/github-logo.png');
 	github.style.display = 'none';
 }
-//Twitter
+
+// Twitter
 function t_hover(element, github, linkedin, twitter, me)
 {
 	element.setAttribute('src', 'img/logos/twitter-logo2.png');
@@ -30,9 +31,9 @@ function t_unhover(element, twitter)
 {
 	element.setAttribute('src', 'img/logos/twitter-logo.png');
 	twitter.style.display = 'none';
-
 }
-//Linkedin
+
+// Linkedin
 function l_hover(element, github, linkedin, twitter, me)
 {
 	element.setAttribute('src', 'img/logos/linkedin-logo2.png');
@@ -48,6 +49,7 @@ function l_unhover(element, linkedin)
 	linkedin.style.display = 'none';
 }
 
+// Noel Negusse
 function me_hover(me)
 {
 	me.style.display = 'block';
@@ -61,78 +63,306 @@ function me_unhover(me)
 
 /* ##### PROJECT ICONS ##### */
 
-//Space Invaders icon
+// ascii invaders
 function si_hover(element)
 {
 	element.setAttribute('src', 'img/projects/space_invader2.jpg');
 }
 
-function si_unhover(element)
-{
-	element.setAttribute('src', 'img/projects/space_invader.jpg');
-}
-//gear
+// kernel vm
 function gear_hover(element)
 {
 	element.setAttribute('src', 'img/projects/gear2.jpg');
 }
 
-function gear_unhover(element)
+// foids
+function fish_hover(element)
 {
-	element.setAttribute('src', 'img/projects/gear.jpg');
-}
-//finger poke
-function poke_hover(element)
-{
-	element.setAttribute('src', 'img/projects/poke2.png');
+	element.setAttribute('src', 'img/projects/fish2.png');
 }
 
-function poke_unhover(element)
+// processor
+function fpga_hover(element)
 {
-	element.setAttribute('src', 'img/projects/poke.png');
-}
-//microchip
-function microchip_hover(element)
-{
-	element.setAttribute('src', 'img/projects/microchip2.png');
+	element.setAttribute('src', 'img/projects/fpga2.png');
 }
 
-function microchip_unhover(element)
+// carnotke
+function hdd_hover(element)
 {
-	element.setAttribute('src', 'img/projects/microchip.png');
+	element.setAttribute('src', 'img/projects/db2.png');
+}
+
+// blockchain database
+function ledger_hover(element)
+{
+	element.setAttribute('src', 'img/projects/ledger2.png');
+}
+
+// gameboy
+function gb_hover(element)
+{
+	element.setAttribute('src', 'img/projects/gb2.png');
+}
+
+// ray tracer
+function ray_hover(element)
+{
+	element.setAttribute('src', 'img/projects/ray2.png');
+}
+
+// smash the stack
+function bucket_hover(element)
+{
+	element.setAttribute('src', 'img/projects/bucket2.jpg');
+}
+
+// device
+function gyro_hover(element)
+{
+	element.setAttribute('src', 'img/projects/gyro2.png');
 }
 
 /* ##### DESCRIPTIONS ##### */
-function ascii_hover(ascii, vm, autopoke, device)
+function ascii_hover(ascii, vm, sensor, device, fish, database, bucket, fpga, ray, gb,
+ascii_img, vm_img, sensor_img, device_img, fish_img, fpga_img, ray_img, gb_img, database_img, bucket_img)
 {
 	ascii.style.display='inline';
 	vm.style.display='none';
-	autopoke.style.display='none';
+	sensor.style.display='none';
 	device.style.display='none';
+	fish.style.display='none';
+	database.style.display='none';
+	bucket.style.display='none';
+	fpga.style.display='none';
+	ray.style.display='none';
+	gb.style.display='none';
+	vm_img.setAttribute('src', 'img/projects/gear.jpg');
+	sensor_img.setAttribute('src', 'img/projects/gyro.png');
+	device_img.setAttribute('src', 'img/projects/ledger.png');
+	fish_img.setAttribute('src', 'img/projects/fish.png');
+	fpga_img.setAttribute('src', 'img/projects/fpga.png');
+	ray_img.setAttribute('src', 'img/projects/ray.png');
+	gb_img.setAttribute('src', 'img/projects/gb.png');
+	database_img.setAttribute('src', 'img/projects/db.png');
+	bucket_img.setAttribute('src', 'img/projects/bucket.jpg');
 }
 
-function vm_hover(ascii, vm, autopoke, device)
+function vm_hover(ascii, vm, sensor, device, fish, database, bucket, fpga, ray, gb,
+ascii_img, vm_img, sensor_img, device_img, fish_img, fpga_img, ray_img, gb_img, database_img, bucket_img)
 {
 	ascii.style.display='none';
 	vm.style.display='inline';
-	autopoke.style.display='none';
+	sensor.style.display='none';
 	device.style.display='none';
+	fish.style.display='none';
+	database.style.display='none';
+	bucket.style.display='none';
+	fpga.style.display='none';
+	ray.style.display='none';
+	gb.style.display='none';
+	ascii_img.setAttribute('src', 'img/projects/space_invader.jpg');
+	sensor_img.setAttribute('src', 'img/projects/gyro.png');
+	device_img.setAttribute('src', 'img/projects/ledger.png');
+	fish_img.setAttribute('src', 'img/projects/fish.png');
+	fpga_img.setAttribute('src', 'img/projects/fpga.png');
+	ray_img.setAttribute('src', 'img/projects/ray.png');
+	gb_img.setAttribute('src', 'img/projects/gb.png');
+	database_img.setAttribute('src', 'img/projects/db.png');
+	bucket_img.setAttribute('src', 'img/projects/bucket.jpg');
 }
 
-function autopoke_hover(ascii, vm, autopoke, device)
+function foids_hover(ascii, vm, sensor, device, fish, database, bucket, fpga, ray, gb,
+ascii_img, vm_img, sensor_img, device_img, fish_img, fpga_img, ray_img, gb_img, database_img, bucket_img)
 {
 	ascii.style.display='none';
 	vm.style.display='none';
-	autopoke.style.display='inline';
+	sensor.style.display='none';
 	device.style.display='none';
+	fish.style.display='inline';
+	database.style.display='none';
+	bucket.style.display='none';
+	fpga.style.display='none';
+	ray.style.display='none';
+	gb.style.display='none';
+	ascii_img.setAttribute('src', 'img/projects/space_invader.jpg');
+	vm_img.setAttribute('src', 'img/projects/gear.jpg');
+	sensor_img.setAttribute('src', 'img/projects/gyro.png');
+	device_img.setAttribute('src', 'img/projects/ledger.png');
+	fpga_img.setAttribute('src', 'img/projects/fpga.png');
+	ray_img.setAttribute('src', 'img/projects/ray.png');
+	gb_img.setAttribute('src', 'img/projects/gb.png');
+	database_img.setAttribute('src', 'img/projects/db.png');
+	bucket_img.setAttribute('src', 'img/projects/bucket.jpg');
 }
 
-function device_hover(ascii, vm, autopoke, device)
+function proc_hover(ascii, vm, sensor, device, fish, database, bucket, fpga, ray, gb,
+ascii_img, vm_img, sensor_img, device_img, fish_img, fpga_img, ray_img, gb_img, database_img, bucket_img)
 {
 	ascii.style.display='none';
 	vm.style.display='none';
-	autopoke.style.display='none';
+	sensor.style.display='none';
+	device.style.display='none';
+	fish.style.display='none';
+	database.style.display='none';
+	bucket.style.display='none';
+	fpga.style.display='inline';
+	ray.style.display='none';
+	gb.style.display='none';
+	ascii_img.setAttribute('src', 'img/projects/space_invader.jpg');
+	vm_img.setAttribute('src', 'img/projects/gear.jpg');
+	sensor_img.setAttribute('src', 'img/projects/gyro.png');
+	device_img.setAttribute('src', 'img/projects/ledger.png');
+	fish_img.setAttribute('src', 'img/projects/fish.png');
+	ray_img.setAttribute('src', 'img/projects/ray.png');
+	gb_img.setAttribute('src', 'img/projects/gb.png');
+	database_img.setAttribute('src', 'img/projects/db.png');
+	bucket_img.setAttribute('src', 'img/projects/bucket.jpg');
+}
+
+function db_hover(ascii, vm, sensor, device, fish, database, bucket, fpga, ray, gb,
+ascii_img, vm_img, sensor_img, device_img, fish_img, fpga_img, ray_img, gb_img, database_img, bucket_img)
+{
+	ascii.style.display='none';
+	vm.style.display='none';
+	sensor.style.display='none';
+	device.style.display='none';
+	fish.style.display='none';
+	database.style.display='inline';
+	bucket.style.display='none';
+	fpga.style.display='none';
+	ray.style.display='none';
+	gb.style.display='none';
+	ascii_img.setAttribute('src', 'img/projects/space_invader.jpg');
+	vm_img.setAttribute('src', 'img/projects/gear.jpg');
+	sensor_img.setAttribute('src', 'img/projects/gyro.png');
+	device_img.setAttribute('src', 'img/projects/ledger.png');
+	fish_img.setAttribute('src', 'img/projects/fish.png');
+	fpga_img.setAttribute('src', 'img/projects/fpga.png');
+	ray_img.setAttribute('src', 'img/projects/ray.png');
+	gb_img.setAttribute('src', 'img/projects/gb.png');
+	bucket_img.setAttribute('src', 'img/projects/bucket.jpg');
+}
+
+function bc_hover(ascii, vm, sensor, device, fish, database, bucket, fpga, ray, gb,
+ascii_img, vm_img, sensor_img, device_img, fish_img, fpga_img, ray_img, gb_img, database_img, bucket_img)
+{
+	ascii.style.display='none';
+	vm.style.display='none';
+	sensor.style.display='none';
 	device.style.display='inline';
+	fish.style.display='none';
+	database.style.display='none';
+	bucket.style.display='none';
+	fpga.style.display='none';
+	ray.style.display='none';
+	gb.style.display='none';
+	ascii_img.setAttribute('src', 'img/projects/space_invader.jpg');
+	vm_img.setAttribute('src', 'img/projects/gear.jpg');
+	sensor_img.setAttribute('src', 'img/projects/gyro.png');
+	device_img.setAttribute('src', 'img/projects/ledger2.png');
+	fish_img.setAttribute('src', 'img/projects/fish.png');
+	fpga_img.setAttribute('src', 'img/projects/fpga.png');
+	ray_img.setAttribute('src', 'img/projects/ray.png');
+	gb_img.setAttribute('src', 'img/projects/gb.png');
+	database_img.setAttribute('src', 'img/projects/db.png');
+	bucket_img.setAttribute('src', 'img/projects/bucket.jpg');
+}
+
+function raytracer_hover(ascii, vm, sensor, device, fish, database, bucket, fpga, ray, gb,
+ascii_img, vm_img, sensor_img, device_img, fish_img, fpga_img, ray_img, gb_img, database_img, bucket_img)
+{
+	ascii.style.display='none';
+	vm.style.display='none';
+	sensor.style.display='none';
+	device.style.display='none';
+	fish.style.display='none';
+	database.style.display='none';
+	bucket.style.display='none';
+	fpga.style.display='none';
+	ray.style.display='inline';
+	gb.style.display='none';
+	ascii_img.setAttribute('src', 'img/projects/space_invader.jpg');
+	vm_img.setAttribute('src', 'img/projects/gear.jpg');
+	sensor_img.setAttribute('src', 'img/projects/gyro.png');
+	device_img.setAttribute('src', 'img/projects/ledger.png');
+	fish_img.setAttribute('src', 'img/projects/fish.png');
+	fpga_img.setAttribute('src', 'img/projects/fpga.png');
+	gb_img.setAttribute('src', 'img/projects/gb.png');
+	database_img.setAttribute('src', 'img/projects/db.png');
+	bucket_img.setAttribute('src', 'img/projects/bucket.jpg');
+}
+
+function gameboy_hover(ascii, vm, sensor, device, fish, database, bucket, fpga, ray, gb,
+ascii_img, vm_img, sensor_img, device_img, fish_img, fpga_img, ray_img, gb_img, database_img, bucket_img)
+{
+	gb.style.display='inline';
+	ascii.style.display='none';
+	vm.style.display='none';
+	sensor.style.display='none';
+	device.style.display='none';
+	fish.style.display='none';
+	database.style.display='none';
+	bucket.style.display='none';
+	fpga.style.display='none';
+	ray.style.display='none';
+	ascii_img.setAttribute('src', 'img/projects/space_invader.jpg');
+	vm_img.setAttribute('src', 'img/projects/gear.jpg');
+	sensor_img.setAttribute('src', 'img/projects/gyro.png');
+	device_img.setAttribute('src', 'img/projects/ledger.png');
+	fish_img.setAttribute('src', 'img/projects/fish.png');
+	fpga_img.setAttribute('src', 'img/projects/fpga.png');
+	ray_img.setAttribute('src', 'img/projects/ray.png');
+	database_img.setAttribute('src', 'img/projects/db.png');
+	bucket_img.setAttribute('src', 'img/projects/bucket.jpg');
+}
+
+function stack_hover(ascii, vm, sensor, device, fish, database, bucket, fpga, ray, gb,
+ascii_img, vm_img, sensor_img, device_img, fish_img, fpga_img, ray_img, gb_img, database_img, bucket_img)
+{
+	ascii.style.display='none';
+	vm.style.display='none';
+	sensor.style.display='none';
+	device.style.display='none';
+	fish.style.display='none';
+	database.style.display='none';
+	bucket.style.display='inline';
+	fpga.style.display='none';
+	ray.style.display='none';
+	gb.style.display='none';
+	ascii_img.setAttribute('src', 'img/projects/space_invader.jpg');
+	vm_img.setAttribute('src', 'img/projects/gear.jpg');
+	sensor_img.setAttribute('src', 'img/projects/gyro.png');
+	device_img.setAttribute('src', 'img/projects/ledger.png');
+	fish_img.setAttribute('src', 'img/projects/fish.png');
+	fpga_img.setAttribute('src', 'img/projects/fpga.png');
+	ray_img.setAttribute('src', 'img/projects/ray.png');
+	gb_img.setAttribute('src', 'img/projects/gb.png');
+	database_img.setAttribute('src', 'img/projects/db.png');
+}
+
+function sensor_hover(ascii, vm, sensor, device, fish, database, bucket, fpga, ray, gb,
+ascii_img, vm_img, sensor_img, device_img, fish_img, fpga_img, ray_img, gb_img, database_img, bucket_img)
+{
+	ascii.style.display='none';
+	vm.style.display='none';
+	sensor.style.display='inline';
+	device.style.display='none';
+	fish.style.display='none';
+	database.style.display='none';
+	bucket.style.display='none';
+	fpga.style.display='none';
+	ray.style.display='none';
+	gb.style.display='none';
+	ascii_img.setAttribute('src', 'img/projects/space_invader.jpg');
+	vm_img.setAttribute('src', 'img/projects/gear.jpg');
+	device_img.setAttribute('src', 'img/projects/ledger.png');
+	fish_img.setAttribute('src', 'img/projects/fish.png');
+	fpga_img.setAttribute('src', 'img/projects/fpga.png');
+	ray_img.setAttribute('src', 'img/projects/ray.png');
+	gb_img.setAttribute('src', 'img/projects/gb.png');
+	database_img.setAttribute('src', 'img/projects/db.png');
+	bucket_img.setAttribute('src', 'img/projects/bucket.jpg');
 }
 
 function ellipsis_hover(element)
@@ -144,3 +374,24 @@ function ellipsis_unhover(element)
 {
 	element.setAttribute('src', 'img/mis/ellipsis.png');
 }
+
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+});
+
+/*
+$(window).on('resize', function() {
+	var win = $(this);
+	if (win.width() >= 1280) {
+
+	}
+	if (win.width() < 1280) {
+
+	}
+});
+*/
+
